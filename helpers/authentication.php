e25a69ed113c9345cef4068897129da51a98f18e
Martin Mbithi <martdevelopers254@gmail.com>
2022-09-28 13:13:38 +0300

    Intergrate Login Backend Handler And Alerts

--- helpers/authentication.php
+++ helpers/authentication.php
@@ -72,7 +72,32 @@ if (isset($_POST['Login'])) {
     $login_password = md5(mysqli_real_escape_string($mysqli, $_POST['login_password']));
 
     /* Process Login */
-    $success = "Tests PASS";
+    $stmt = $mysqli->prepare("SELECT login_username, login_password, login_rank, login_id FROM login 
+    WHERE login_username = '{$login_username}' AND login_password = '{$login_password}'");
+    $stmt->execute();
+    $stmt->bind_result($login_username, $login_password, $login_rank, $login_id);
+    $rs = $stmt->fetch();
+
+    /* Session Variables */
+    $_SESSION['login_id'] = $login_id;
+    $_SESSION['login_rank'] = $login_rank;
+
+    if (($rs && $login_rank == "administrator")) {
+        /* Pass This Alert Via Session */
+        $_SESSION['success'] = 'You Have Successfully Logged In';
+        header('Location: dashboard');
+        exit;
+    } elseif ($rs && $user_access_level == "owner") {
+        $_SESSION['success'] = 'Successfully logged in as pet owner';
+        header('Location: owner_dashboard');
+        exit;
+    } elseif ($rs && $user_access_level == "adopter") {
+        $_SESSION['success'] = 'Successfully logged in as pet adopter';
+        header('Location: adopter_dashboard');
+        exit;
+    } else {
+        $err = "Access Denied Please Check Your Email Or Password";
+    }
 }
 
  /* Sign Up As Pet Adopter */
