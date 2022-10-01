1a390554615731c2a9b51be1398ae0d3b766cc04
Martin Mbithi <martdevelopers254@gmail.com>
2022-10-01 09:39:37 +0300

    Implement Pet Adopter & Pet Owner Account Creation Backend Handler

--- helpers/authentication.php
+++ helpers/authentication.php
@@ -130,8 +130,8 @@ if (isset($_POST['Register_Pet_Adopter'])) {
             $login_id = $mysqli->insert_id;
 
             /* Persist Adopter Record */
-            $adopter_sql = "INSERT INTO pet_adopter(pet_adopter_name, pet_adopter_email, pet_adopter_phone_number, pet_adopter_address)
-            VALUES('{$pet_adopter_name}', '{$pet_adopter_email}', '{$pet_adopter_phone_number}', '{$pet_adopter_address}')";
+            $adopter_sql = "INSERT INTO pet_adopter(pet_adopter_login_id, pet_adopter_name, pet_adopter_email, pet_adopter_phone_number, pet_adopter_address)
+            VALUES('{$login_id}', '{$pet_adopter_name}', '{$pet_adopter_email}', '{$pet_adopter_phone_number}', '{$pet_adopter_address}')";
 
             if (mysqli_query($mysqli, $adopter_sql)) {
                 /* Redirect To Login After Successful Sign Up */
@@ -146,6 +146,50 @@ if (isset($_POST['Register_Pet_Adopter'])) {
 }
 
 /* Sign Up As Pet Owner */
+if (isset($_POST['Register_Pet_Owner'])) {
+    $pet_owner_name = mysqli_real_escape_string($mysqli, $_POST['pet_owner_name']);
+    $pet_owner_email = mysqli_real_escape_string($mysqli, $_POST['pet_owner_email']);
+    $pet_owner_contacts = mysqli_real_escape_string($mysqli, $_POST['pet_owner_contacts']);
+    $pet_owner_address = mysqli_real_escape_string($mysqli, $_POST['pet_owner_address']);
+
+    /* Auth Variables */
+    $login_username = mysqli_real_escape_string($mysqli, $_POST['login_username']);
+    $login_password = md5(mysqli_real_escape_string($mysqli, $_POST['login_password']));
+    $login_rank = mysqli_real_escape_string($mysqli, $_POST['Owner']);
+
+    /* Prevent Double submissions */
+    $sql = "SELECT * FROM  login   WHERE login_username = '{$login_username}'";
+    $res = mysqli_query($mysqli, $sql);
+    if (mysqli_num_rows($res) > 0) {
+        $row = mysqli_fetch_assoc($res);
+        if (
+            $login_username == $row['login_username']
+        ) {
+            $err = 'Login username already taken';
+        }
+    } else {
+        /* Persist Auth Record */
+        $auth_sql = "INSERT INTO login (login_username, login_password, login_rank) VALUES('{$login_username}', '{$login_password}', '{$login_rank}')";
+        if (mysqli_query($mysqli, $auth_sql)) {
+            /* Get Newly Inserted Login Id */
+            $login_id = $mysqli->insert_id;
+
+            /* Persist Owner Record */
+            $owner_sql = "INSERT INTO pet_owner(pet_owner_login_id, pet_owner_name, pet_owner_email, pet_owner_contacts, pet_owner_address)
+            VALUES('{$pet_owner_login_id}', '{$pet_owner_name}', '{$pet_owner_email}', '{$pet_owner_contacts}', '{$pet_owner_address}')";
+
+            if (mysqli_query($mysqli, $owner_sql)) {
+                /* Redirect To Login After Successful Sign Up */
+                $_SESSION['success'] = 'Account created successfully';
+                header('Location: ../');
+                exit;
+            }
+        } else {
+            $err = "Failed saving login information, please try again";
+        }
+    }
+}
+
 
 /* Reset Password Step 1 */
 if (isset($_POST['Reset_Password_Step_1'])) {
