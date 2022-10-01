99208c4ff1cae433489c36fb61c2fa5bcc60c4c6
Martin Mbithi <martdevelopers254@gmail.com>
2022-10-01 09:21:37 +0300

    Craft Pet Adopter Backend Authentication Helper

--- helpers/authentication.php
+++ helpers/authentication.php
@@ -101,6 +101,38 @@ if (isset($_POST['Login'])) {
 }
 
 /* Sign Up As Pet Adopter */
+if (isset($_POST['Register_Pet_Adopter'])) {
+    $pet_adopter_name = mysqli_real_escape_string($mysqli, $_POST['pet_adopter_name']);
+    $pet_adopter_email = mysqli_real_escape_string($mysqli, $_POST['pet_adopter_email']);
+    $pet_adopter_phone_number = mysqli_real_escape_string($mysqli, $_POST['pet_adopter_phone_number']);
+    $pet_adopter_address = mysqli_real_escape_string($mysqli, $_POST['pet_adopter_address']);
+
+    /* Auth Variables */
+    $login_username = mysqli_real_escape_string($mysqli, $_POST['login_username']);
+    $login_password = md5(mysqli_real_escape_string($mysqli, $_POST['login_password']));
+    $login_rank = mysqli_real_escape_string($mysqli, $_POST['Adopter']);
+
+    /* Prevent Double submissions */
+    $sql = "SELECT * FROM  login   WHERE login_username = '{$login_username}'";
+    $res = mysqli_query($mysqli, $sql);
+    if (mysqli_num_rows($res) > 0) {
+        $row = mysqli_fetch_assoc($res);
+        if (
+            $login_username == $row['login_username']
+        ) {
+            $err = 'Login username already taken';
+        }
+    } else {
+        /* Persist Auth Record */
+        $auth_sql = "INSERT INTO login (login_username, login_password, login_rank) VALUES('{$login_username}', '{$login_password}', '{$login_rank}')";
+        if (mysqli_query($mysqli, $auth_sql)) {
+            /* Get Newly Inserted Login Id */
+            $login_id = $mysqli->insert_id;
+
+            /* Persist  */
+        }
+    }
+}
 
 /* Sign Up As Pet Owner */
 
@@ -117,7 +149,7 @@ if (isset($_POST['Reset_Password_Step_1'])) {
         header('Location: confirm_password');
         exit;
     } else {
-        $err = "Nationa ID Number Does Not Exist";
+        $err = "Email Does Not Exist";
     }
 }
 
