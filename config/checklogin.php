df24c56f226c17fb181b649b7cd893e50a5e0d6e
Martin Mbithi <martdevelopers254@gmail.com>
2022-10-02 08:52:45 +0300

    Use Login Id To Persist Auth Session

--- config/checklogin.php
+++ config/checklogin.php
@@ -72,11 +72,11 @@
 function check_login()
 {
 	/* Use User Id As Session */
-	if ((strlen($_SESSION['user_id']) == 0)) {
+	if ((strlen($_SESSION['login_id']) == 0)) {
 		$host = $_SERVER['HTTP_HOST'];
 		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 		$extra = "index";
-		$_SESSION["user_id"] = "";
+		$_SESSION["login_id"] = "";
 		header("Location: http://$host$uri/$extra");
 	}
 }
