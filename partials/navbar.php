60921213872bc8220f27ff5f85fe6cf8d0f368e3
Martin Mbithi <martdevelopers254@gmail.com>
2022-10-06 08:54:39 +0300

    Add Log Out Confirmation Modal

--- partials/navbar.php
+++ partials/navbar.php
@@ -13,9 +13,32 @@
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-            <a class="nav-link" href="logout">
+            <a class="nav-link" data-toggle="modal" href="#logout_modal">
                 <i class="fas fa-user-tag"></i>
             </a>
         </li>
     </ul>
 </nav>
+
+<!-- Logout Modal -->
+<div class="modal fade" id="logout_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">CONFIRM LOGOUT</h5>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+            <form method="POST" action="logout">
+                <div class="modal-body text-center text-danger">
+                    <h4>Terminate Session?</h4>
+                    <br>
+                    <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
+                    <input type="submit" value="Yes, Logout" class="text-center btn btn-danger">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- End Modal -->
