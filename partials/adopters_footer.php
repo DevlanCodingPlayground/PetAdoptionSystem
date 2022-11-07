adf6de6f7ed5e37821063e8ae61f2a4ae7b99c99
Martin Mbithi <martdevelopers254@gmail.com>
2022-11-07 15:14:55 +0300

    Scaffold Adopters Dashboard

---
+++ partials/adopters_footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+        iPet, Online Pet Adoption System
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; <?php echo date('Y'); ?> <a href="http://devlan.co.ke/">Devlan Solutions LTD</a>.</strong>
+    All rights reserved.
+</footer>
