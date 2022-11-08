57f299e2c0c11cfe3bb265d2bc503dd209ec1f5c
Martin Mbithi <martdevelopers254@gmail.com>
2022-11-08 15:51:06 +0300

    Craft Owner Adoptions Details

--- views/owner_dashboard.php
+++ views/owner_dashboard.php
@@ -127,7 +127,7 @@ require_once('../partials/head.php');
                                 <div class="icon">
                                     <i class="fas fa-paw"></i>
                                 </div>
-                                <a href="owner_adoptions" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                <a href="owner_pets" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                             </div>
                         </div>
                         <div class="col-lg-4 col-6">
