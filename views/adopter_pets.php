5ac021d57188abb733b78291854f7ae6428d3689
Martin Mbithi <martdevelopers254@gmail.com>
2022-11-14 20:48:38 +0300

    Discovered How To Add Dark Mode

--- views/adopter_pets.php
+++ views/adopter_pets.php
@@ -112,7 +112,7 @@ require_once('../partials/head.php');
                         <!-- Info boxes -->
                         <div class="row">
                             <?php
-                            $per_page_record = 12;
+                            $per_page_record = 4;
                             if (isset($_GET["page"])) {
                                 $page  = $_GET["page"];
                             } else {
--- views/pets.php
+++ views/pets.php
@@ -193,7 +193,7 @@ require_once('../partials/head.php');
                     <!-- Info boxes -->
                     <div class="row">
                         <?php
-                        $per_page_record = 12;
+                        $per_page_record = 4;
                         if (isset($_GET["page"])) {
                             $page  = $_GET["page"];
                         } else {
